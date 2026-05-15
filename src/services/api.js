af321fb615e112d5e4f9a27bd3650769a0f8de5a
imayush36 <amsp78810@gmail.com>
2026-05-16 00:06:31 +0530

    Final Mega Mart Website

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,3 @@
-import axios from "axios"
+const API_URL = "http://localhost:5000";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000"
-})
-
-export default API
+export default API_URL;
